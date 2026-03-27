53cd1f72e663f4f06e36e7c0d51d7fe70194c025
Felipe <felipenule30@gmail.com>
2026-03-26 20:05:56 -0500

    SEO: añadir archivo verificacion Google Search Console

--- greendome/urls.py
+++ greendome/urls.py
@@ -12,6 +12,13 @@ sitemaps = {
 }
 
 
+def google_verification(request):
+    return HttpResponse(
+        'google-site-verification: google5bc53e07450d12e0.html',
+        content_type='text/html'
+    )
+
+
 def robots_txt(request):
     lines = [
         'User-agent: *',
@@ -27,6 +34,7 @@ def robots_txt(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', robots_txt),
+    path('google5bc53e07450d12e0.html', google_verification),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
